0c274121b0616d15707494565c9d35738929a3c3
Tonin <toninagaj1@gmail.com>
2024-10-18 17:00:35 +0200

    added variables lists

---
+++ module1/main.py
@@ -0,0 +1,31 @@
+emri="Puhiza"
+mosha=50
+temperature=25.5
+
+print(emri) # Puhiza
+
+print(type(emri))
+print(type(mosha))
+print(type(temperature))
+
+mosha = mosha+10
+
+print (mosha)
+
+emri_mbiemri=emri+" Rexha"
+
+print(emri_mbiemri)
+
+my_data=["Tonin","Agaj",23,"Shkolla Digjitale"]
+print(my_data)
+my_data.append("Prishtine")
+print(my_data)
+my_data.insert(4,"Klasa 12")
+print(my_data)
+
+my_data.remove("Tonin")
+
+del my_data[0]
+
+print(my_data)
+
